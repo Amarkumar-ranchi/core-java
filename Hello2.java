69e32b15b3a22379137fed60f41e147f5cc1f484
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-10 16:18:58 +0530

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,7 @@
+class Hello2 {
+public static void main (String as []){
+System.out.println("hello jlc guys");
+System.out.println("hello student of jlc");
+System.out.println("hello team members of jlc");
+}
+}
