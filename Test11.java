3cbf4563dd40f0815b97ced47634c6f406aaf80b
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:51:42 +0530

    Add files via upload

---
+++ Test11.java
@@ -0,0 +1,6 @@
+class Test11{
+public static void main(String as[]){
+String string ="JLC";
+System.out.println(string);
+}
+}
---
+++ Test12.java
@@ -0,0 +1,16 @@
+/*class Test12{
+public static void main(String as[]){
+char ch;
+System.out.println(ch == 0);
+}
+}*/
+
+
+ class Test12 {
+    public static void main(String as[]) {
+        char ch = 0;  // Initialize the char variable
+        System.out.println(ch == 0);  // This will print 'true'
+    }
+}
+
+
