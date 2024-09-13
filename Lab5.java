ac522b6954e71ac5699f1db3aaf29298aba229c2
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:09:20 +0530

    Add files via upload

---
+++ Lab5.java
@@ -0,0 +1,9 @@
+class Lab5{
+ int a;
+public static void main(String[]as){
+System.out.println(a);
+}
+}
+
+/* error: non-static variable a cannot be referenced from a static context
+System.out.println(a);*/
