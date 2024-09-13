b2ef797e140ba0cf31d1919a5ab3f26b62be77c7
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:14:51 +0530

    Add files via upload

---
+++ Lab17.java
@@ -0,0 +1,33 @@
+/*class Lab17{
+public static void main (String as[]){
+final int a=18;
+System.out.println(a);
+System.out.println(a+90);
+}
+}*/
+
+/*output:18
+       108*/
+
+
+
+class Lab17{
+public static void main (String as[]){s
+final int a=18;
+System.out.println(a);
+System.out.println(a+90);
+System.out.println(a-9);
+System.out.println(a/9);
+
+}
+}
+
+/*output:18
+       108
+       9
+       2*/
+
+
+
+
+
