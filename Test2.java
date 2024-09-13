a3c80a6571d4775818961762edcbeddbcffe3ea3
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:15:50 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,22 @@
+/*class Test2{
+public static void main(String as[]){
+char char value='A';
+System.out.println(char value);
+}
+}*/
+
+/* error: not a statement
+char char value='A';
+
+ error: ';' expected
+char char value='A';
+*/
+
+
+class Test2{
+public static void main(String as[]){
+char value='A';
+System.out.println( value);
+}
+}
+
