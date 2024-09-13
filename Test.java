9dd9cbb0216c4bd714fc84333f63aec6942439e2
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:15:23 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test1{
+public static void main(String as []){
+int enum=9;
+System.out.println(enum);
+}
+}
+
+
+
+/* error: as of release 5, 'enum' is a keyword, and may not be used as an identifier
+int enum=9;*/
