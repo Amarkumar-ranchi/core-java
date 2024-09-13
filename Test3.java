f146de8fba6861f56b6f2d53fa8248d2fc119e10
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:16:28 +0530

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,19 @@
+/*class Test3{
+public static void main(String as[]){
+byte b=128;
+System.out.println(b);
+}
+}*/
+
+/* error: incompatible types: possible lossy conversion from int to byte
+byte b=128;*/
+ 
+
+class Test3{
+public static void main(String as[]){
+byte b=127;
+System.out.println(b);
+}
+}
+
+      
