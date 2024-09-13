ec5c3dfa7395ecd0279a6650d981bdca3663bc7e
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-13 17:11:19 +0530

    Add files via upload

---
+++ Lab10.java
@@ -0,0 +1,26 @@
+/*class Lab10{
+public static void main(String as[]){
+int a=120,b=76,c=97;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+String str1="jlc",str2="SRI",str3="NIVASAN";
+System.out.println(str1);
+System.out.println(str2);
+System.out.println(str3);
+} 
+}*/
+
+
+class Lab10{
+public static void main(String as[]){
+int a=120,b=76,c=97;
+String str1="jlc",str2="SRI",str3="NIVASAN";
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(str1);
+System.out.println(str2);
+System.out.println(str3);
+} 
+}
