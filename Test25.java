48794db177b4b70d93dd50ad1ea724be41406c28
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2024-09-14 15:49:36 +0530

    Add files via upload

---
+++ Test25.java
@@ -0,0 +1,6 @@
+class Test25{
+public static void main(String as []){
+final int A;
+System.out.println('A');
+}
+}
---
+++ Test26.java
@@ -0,0 +1,10 @@
+class Test26{
+public static void main(String jlc[]){
+static int a;
+System.out.println(a);
+}
+}
+
+
+/* error: illegal start of expression
+static int a;*/
