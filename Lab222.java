6fdcc8d50bb1c7a77824b0cece2d79eda056d4db
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2025-03-20 16:31:34 +0530

    Add files via upload

---
+++ Lab222.java
@@ -0,0 +1,16 @@
+class Lab222 {
+public static void main(String jlc[]){
+int arr[] = new int[5] {10,20,30,40,50,60};
+for(int i=0;i<arr.length;i++)
+System.out.println(arr[i]);
+}
+} 
+
+ /*error: ';' expected
+int arr[] = new int[5] {10,20,30,40,50,60};
+                      
+ error: not a statement
+int arr[] = new int[5] {10,20,30,40,50,60};
+                        
+ error: ';' expected
+int arr[] = new int[5] {10,20,30,40,50,60};*/
