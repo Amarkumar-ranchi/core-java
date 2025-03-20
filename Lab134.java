1b3e082cd965b0215a4448cf1ad36a01967c87ec
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2025-03-20 16:10:07 +0530

    Add files via upload

---
+++ Lab134.java
@@ -0,0 +1,13 @@
+class Lab134{
+public static void main(String as[]){
+if(true)
+System.out.println("IF BLOCK 1");
+System.out.println("HELLO GUYS");
+if(false)
+System.out.println("IF BLOCK 2");
+System.out.println("HAI GUYS");
+}
+}
+
+
+
